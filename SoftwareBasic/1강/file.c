87465ef30ad624b21e17d404ca827a0407faf431
Linyera <57083072+Lazencas@users.noreply.github.com>
2022-04-26 22:26:55 +0900

    2 commit
    새 예제 추가

--- SoftwareBasic/1강/file.c
+++ SoftwareBasic/1강/file.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
 
+typedef struct {
+	char name[20];
+    int score;
+} Student;
 
 
 int main(void) {
@@ -11,14 +15,22 @@ int main(void) {
 	파일열기 > 파일일기/쓰기 > 파일닫기
 	
 	*/
-	char s[20] = "Hello World";
-	FILE *fp;
-	fp = fopen("temp.txt", "w");
-	fprintf(fp, "%s\n", s);
-	fclose(fp);
+	//char s[20] = "Hello World";
+	//FILE *fp;
+	//fp = fopen("temp.txt", "w");
+	//fprintf(fp, "%s\n", s);
+	//fclose(fp);
 
-
-
+	int n, sum = 0;
+	FILE* fp;
+	fp = fopen("input.txt", "r");
+	fscanf(fp, "%d", &n);
+	Student *students = (Student*)malloc(sizeof(Student) * n);
+	for (int i = 0; i < n; i++) {
+		fscanf(fp, "%s %d", &((students + i)->name), &((students + i)->score));
+		printf("이름: %s, 성적: %d\n", (students + i)->name, (students + i)->score);
+	}
+	system("pause");
 	
 	return 0;
 }
