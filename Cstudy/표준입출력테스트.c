f1b6fe7f1e8b63fe43b77cc5bc1338821e74c3ab
GunTae Kim <57083072+Lazencas@users.noreply.github.com>
2022-12-13 13:01:13 +0900

    Add files via upload

---
+++ Cstudy/표준입출력테스트.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdint.h> 
+/*
+예제 
+
+*/
+
+/*----------------- Function declaration -----------------*/
+int main(void); //메인함수  
+
+/*--------------- Function declaration End ---------------*/
+
+
+int main(void){
+    uint8_t ch = 0;
+    ch = getchar();
+	putchar(ch);	
+	return 0;
+} 
